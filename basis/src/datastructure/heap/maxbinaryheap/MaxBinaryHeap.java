e97e6217cc7b7e4911242e0c951412797b377ff7
YihaoChan <945798214@qq.com>
2021-01-13 15:13:38 +0800

    优化程序；补充哨兵笔记

--- basis/src/datastructure/heap/maxbinaryheap/MaxBinaryHeap.java
+++ basis/src/datastructure/heap/maxbinaryheap/MaxBinaryHeap.java
@@ -3,36 +3,37 @@ package datastructure.heap.maxbinaryheap;
 /**
  * @Description: 最大堆(大顶堆)
  */
-public class MaxBinaryHeap<T extends Comparable<T>> {
+public class MaxBinaryHeap {
     // 存储堆中元素的数组
-    private T[] heap;
+    private Integer[] heap;
 
     // 堆中元素个数，同时也是数组中最后一个元素的索引(因为0号索引为哨兵，不参与数组计算)
-    private int n;
+    private int size;
 
     /* 构造方法 */
     public MaxBinaryHeap(int capacity) {
-        this.heap = (T[]) new Comparable[++capacity]; // 0号位置为哨兵，所以要多申请一个空间
-        this.n = 0;
+        this.heap = new Integer[++capacity]; // 0号位置为哨兵，所以要多申请一个空间
+        this.heap[0] = Integer.MAX_VALUE; // 哨兵定义为正无穷，大于堆中的所有元素
+        this.size = 0;
     }
 
     /* 获取堆中元素数量 */
     public int size() {
-        return n;
+        return size;
     }
 
     /**
      * @Description: 向堆中插入一个元素
      */
-    public void insert(T t) {
+    public void insert(int t) {
         // 数组中存放的最后一个元素的索引+1，用于存放新元素
-        n++;
+        size++;
 
         // 将新元素放入数组
-        heap[n] = t;
+        heap[size] = t;
 
         // 根据新添加的元素索引，使新添加的元素通过上滤找到插入的合适位置
-        percolateUp(n);
+        percolateUp(size);
     }
 
     /**
@@ -40,37 +41,39 @@ public class MaxBinaryHeap<T extends Comparable<T>> {
      * 在数组的末端插入元素，并使该元素通过上浮正确插入到堆中，以保持堆的秩序
      */
     private void percolateUp(int k) {
-        // 如果添加的新结点的元素大于其父结点的元素，则交换位置，直至父结点元素大于等于子结点为止
-        // 由于0号元素为哨兵，所以索引k不能碰到0。由于k会进行k / 2操作，因此k本身需要大于1
-        T temp;
+        /*
+         * 如果添加的新结点的元素大于其父结点的元素，则交换位置，直至父结点元素大于等于子结点为止.
+         * 1. 设置哨兵为正无穷的好处在于：如果没有哨兵，则每一次都要判断当前调整的元素是否为堆顶元素，
+         *    即每一次都要判断下标是否为0。如果调整的元素是堆顶元素，调整后直接结束。
+         *    但是这样子每一次除了要判断结点的大小关系外，还要进行下标的判断，很浪费时间。
+         * 2. 所以设置哨兵后，直到发现当前结点小于父结点时，有两种情况：
+         *    2.1 还没调整到堆顶结点时就已经调整完毕；
+         *    2.2 遇到了正无穷，即调整到了堆顶。
+         *    设置哨兵后，就不需要每一次再多去判断下标是否碰到0，只需要比较结点的大小即可，节省时间。
+         */
+        while (heap[k].compareTo(heap[k / 2]) > 0) {
+            // 交换新元素和父结点元素
+            Integer temp = heap[k / 2];
+            heap[k / 2] = heap[k];
+            heap[k] = temp;
 
-        while (k > 1) {
-            if (heap[k].compareTo(heap[k / 2]) > 0) {
-                // 交换新元素和父结点元素
-                temp = heap[k / 2];
-                heap[k / 2] = heap[k];
-                heap[k] = temp;
-
-                // k步进，让添加的元素上浮，继续进行判断
-                k = k / 2;
-            } else {
-                break;
-            }
+            // k步进，让添加的元素上浮，继续进行判断
+            k = k / 2;
         }
     }
 
     /**
      * @Description: 从堆中删除最大元素，即堆顶元素，并返回该元素
      */
-    public T deleteMax() {
+    public int deleteMax() {
         // 0号元素为哨兵，最大堆的1号元素为删除的最大元素
-        T maxEle = heap[1];
+        int maxEle = heap[1];
 
         // 拿出最大元素，并通过下滤调整其他元素的位置，使堆保持原有秩序
         percolateDown(1);
 
         // 元素数量减1
-        n--;
+        size--;
 
         return maxEle;
     }
@@ -84,8 +87,8 @@ public class MaxBinaryHeap<T extends Comparable<T>> {
         int largerChildIndex;
 
         // 让被移动的元素的子结点中的较大者移动到原来的父结点处
-        while (k <= n || k + 1 <= n) {
-            if (2 * k > n || 2 * k + 1 > n) {
+        while (k <= size || k + 1 <= size) {
+            if (2 * k > size || 2 * k + 1 > size) {
                 // 子结点下标超过堆的长度，说明已经下滤到最底层，跳出
                 break;
             }
@@ -114,7 +117,7 @@ public class MaxBinaryHeap<T extends Comparable<T>> {
         }
 
         // 如果两个子结点都为空，说明已经下滤到最底层，则将数组末端元素移动到该位置进行填充，并将数组末端元素置空
-        heap[k] = heap[n];
-        heap[n] = null;
+        heap[k] = heap[size];
+        heap[size] = null;
     }
 }
--- basis/src/datastructure/heap/maxbinaryheap/MaxBinaryHeapTest.java
+++ basis/src/datastructure/heap/maxbinaryheap/MaxBinaryHeapTest.java
@@ -4,9 +4,9 @@ package datastructure.heap.maxbinaryheap;
  * @Description: 最大堆(大顶堆) - 测试类
  */
 public class MaxBinaryHeapTest {
-    private static void testEgOne() {
+    public static void main(String[] args) {
         // 《数据结构与算法分析》 P159
-        MaxBinaryHeap<Integer> maxHeap = new MaxBinaryHeap<>(11);
+        MaxBinaryHeap maxHeap = new MaxBinaryHeap(11);
 
         /* 插入元素 */
         maxHeap.insert(32);
@@ -25,55 +25,5 @@ public class MaxBinaryHeapTest {
         while (maxHeap.size() != 0) {
             System.out.print(maxHeap.deleteMax() + "\t");
         }
-    }
-
-    private static void testEgTwo() {
-        MaxBinaryHeap<String> maxHeap = new MaxBinaryHeap<>(11);
-
-        /* 插入元素 */
-        maxHeap.insert("G");
-        maxHeap.insert("I");
-        maxHeap.insert("H");
-        maxHeap.insert("E");
-        maxHeap.insert("O");
-        maxHeap.insert("A");
-        maxHeap.insert("N");
-        maxHeap.insert("P");
-        maxHeap.insert("S");
-        maxHeap.insert("R");
-        maxHeap.insert("T");
-
-        /* 删除顶端最大元素并查看 */
-        while (maxHeap.size() != 0) {
-            System.out.print(maxHeap.deleteMax() + "\t");
-        }
-    }
-
-    private static void testEgThree() {
-        MaxBinaryHeap<String> maxHeap = new MaxBinaryHeap<>(6);
-
-        /* 插入元素 */
-        maxHeap.insert("B");
-        maxHeap.insert("C");
-        maxHeap.insert("A");
-        maxHeap.insert("E");
-        maxHeap.insert("F");
-        maxHeap.insert("D");
-
-        /* 删除顶端最大元素并查看 */
-        while (maxHeap.size() != 0) {
-            System.out.print(maxHeap.deleteMax() + "\t");
-        }
-    }
-    public static void main(String[] args) {
-        testEgOne();
-
-        System.out.println();
-
-        testEgTwo();
-
-        System.out.println();
-
-        testEgThree();
     }
 }
