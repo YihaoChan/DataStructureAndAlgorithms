2249b7f27274cb3507c72debf990039162ccec30
YihaoChan <945798214@qq.com>
2021-01-19 18:08:34 +0800

    优化程序，修改计算哈夫曼编码方式

--- basis/src/datastructure/tree/huffmantree/HuffmanTree.java
+++ basis/src/datastructure/tree/huffmantree/HuffmanTree.java
@@ -3,19 +3,18 @@ package datastructure.tree.huffmantree;
 import java.util.*;
 
 /**
- * @Description: 哈夫曼树、哈夫曼编码
+ * @Description: 哈夫曼树及哈夫曼编码
  */
 public class HuffmanTree {
-
     /**
      * @Description: 结点类
      */
-    class Node<T> {
+    class Node {
         // 数据域
-        private T data;
+        private Character data;
 
         // 每个数据所占的权值
-        private double weight;
+        private int weight;
 
         // 左子结点
         private Node left;
@@ -23,284 +22,206 @@ public class HuffmanTree {
         // 右子结点
         private Node right;
 
+        // 哈夫曼编码
+        private String code;
+
         /* 构造方法 */
-        public Node(T data, double weight) {
+        public Node(Character data, int weight) {
             this.data = data;
             this.weight = weight;
+            this.code = "";
         }
+    }
 
-        public double getWeight() {
-            return weight;
+    // 初始哈夫曼树数组容量
+    private int capacity;
+
+    // 二叉树数量
+    private int size;
+
+    /**
+     * @Description: 构造方法
+     */
+    public HuffmanTree(int capacity) {
+        this.capacity = capacity;
+    }
+
+    /**
+     * @Description: 插入二叉树
+     */
+    private void insert(Node[] arr, Node node) {
+        arr[this.size++] = node;
+    }
+
+    /**
+     * @Description: 删除给定下标对应二叉树
+     */
+    private void remove(Node[] arr, int index) {
+        arr[index] = null;
+
+        this.size--;
+    }
+
+    /**
+     * @Description: 快速排序 - 确定枢轴位置
+     */
+    private int partition(Node[] nodes, int low, int high) {
+        Node pivot = nodes[low];
+        
+        while (low < high) {
+            while (low < high && nodes[high].weight <= pivot.weight) {
+                high--;
+            }
+            
+            nodes[low] = nodes[high];
+            
+            while (low < high && nodes[low].weight >= pivot.weight) {
+                low++;
+            }
+            
+            nodes[high] = nodes[low];
         }
+        
+        nodes[low] = pivot;
+        
+        return low;
+    }
 
-        public T getData() {
-            return data;
-        }
+    /**
+     * @Description: 快速排序 - 逻辑
+     */
+    private void quickSort(Node[] arr, int low, int high) {
+        if (low < high) {
+            int pivotLoc = partition(arr, low, high);
 
-        public Node getLeft() {
-            return left;
-        }
+            quickSort(arr, low, pivotLoc); // 递归完成左边
 
-        public Node getRight() {
-            return right;
-        }
-
-        public void setLeft(Node left) {
-            this.left = left;
-        }
-
-        public void setRight(Node right) {
-            this.right = right;
+            quickSort(arr, pivotLoc + 1, high); // 递归完成右边
         }
     }
 
     /**
-     * @Description: 构造哈夫曼树
-     * @param: nodes 根据每个权值构建一棵只有一棵结点的二叉树，并存放在列表中，再根据权值大小对将列表中的结点进行排序
-     * @return: 生成的哈夫曼树的根结点
+     * @Description: 快速排序
      */
-    public List<Node> createHuffmanTree(List<Node> nodes) {
-        while (nodes.size() > 1) { // 直到结点列表中只含有一个结点，即只剩下一棵二叉树，此时为哈夫曼树
-            // 对根据当前列表中结点的权值对结点进行排序
-            quickSort(nodes);
+    private void sort(Node[] arr) {
+        quickSort(arr, 0, this.size - 1);
+    }
 
-            // 取出权值最小的两个结点，作为新的二叉树的左右结点(较大的在左)
-            Node leftChild = nodes.get(nodes.size() - 2);
-            Node rightChild = nodes.get(nodes.size() - 1);
+    /**
+     * @Description: 构建哈夫曼树
+     */
+    private Node buildHuffmanTree(Node[] nodes) {
+        // 直到数组中只含有一棵二叉树时，哈夫曼树形成
+        while (this.size > 1) {
+            // 按二叉树根结点权值从大到小排序
+            sort(nodes);
 
-            // 新的二叉树的根结点
+            // 取出根结点权值最小的两棵二叉树，作为新的二叉树的左右子树(根结点权值较大的在左)
+            Node leftChild = nodes[this.size - 2];
+            Node rightChild = nodes[this.size - 1];
+
+            // 新二叉树
             Node parent = new Node(null, leftChild.weight + rightChild.weight);
             parent.left = leftChild;
             parent.right = rightChild;
 
-            // 在原来数组中删除取出的两个树
-            int temp = nodes.size() - 1; // 保存原来的最小权值索引，否则remove后最小权值索引会发生变化！
-            nodes.remove(temp);
-            nodes.remove(temp - 1);
+            // 删除取出的两棵树
+            remove(nodes, this.size - 1);
+            remove(nodes, this.size - 1);
 
-            // 将新的二叉树加入数组中
-            nodes.add(parent);
+            // 插入新二叉树
+            insert(nodes, parent);
         }
 
-        return nodes;
+        return nodes[0];
     }
 
     /**
-     * @Description: 将列表中i和j索引处的元素进行交换
+     * @Description: 层序遍历计算哈夫曼编码
+     * @param node 哈夫曼树根结点
      */
-    private void swap(List<Node> nodes, int i, int j) {
-        Node temp = nodes.get(j);
-        nodes.set(j, nodes.get(i));
-        nodes.set(i, temp);
-    }
-
-    /**
-     * @Description: 对每个二叉树结点元素进行快速排序
-     */
-    private void subSort(List<Node> nodes, int start, int end) {
-        if (start < end) {
-            // 以第一个元素作为分界值
-            Node base = nodes.get(start);
-
-            // i从左边搜索，搜索大于分界值的元素的索引
-            int i = start;
-
-            // j从右边开始搜索，搜索小于分界值的元素的索引
-            int j = end + 1;
-
-            while (true) {
-                // 找到大于分界值的元素的索引，或者i已经到了end处
-                while (i < end && nodes.get(++i).weight >= base.weight)
-                    ; // 空语句，只执行循环体
-
-                // 找到小于分界值的元素的索引，或者j已经到了start处
-                while (j > start && nodes.get(--j).weight <= base.weight)
-                    ; // 空语句，只执行循环体
-
-                if (i < j) {
-                    swap(nodes, i, j);
-                } else {
-                    break;
-                }
-            }
-
-            swap(nodes, start, j);
-
-            //递归左边子序列
-            subSort(nodes, start, j - 1);
-
-            //递归右边子序列
-            subSort(nodes, j + 1, end);
-        }
-    }
-
-    private void quickSort(List<Node> nodes) {
-        subSort(nodes, 0, nodes.size() - 1);
-    }
-
-    /**
-     * @Description: 前序遍历 - 逻辑
-     */
-    private void preOrderTraversal(Node curr) {
-        if (curr == null) {
-            return;
-        }
-
-        System.out.print(curr.data + "\t");
-        preOrderTraversal(curr.left);
-        preOrderTraversal(curr.right);
-    }
-
-    /**
-     * @Description: 前序遍历
-     */
-    public void preOrderTraversal(List<Node> nodes) {
-        preOrderTraversal(nodes.get(0));
-    }
-
-    /**
-     * @Description: 中序遍历 - 逻辑
-     */
-    private void inOrderTraversal(Node curr) {
-        if (curr == null) {
-            return;
-        }
-
-        inOrderTraversal(curr.left);
-        System.out.print(curr.data + "\t");
-        inOrderTraversal(curr.right);
-    }
-
-    /**
-     * @Description: 中序遍历
-     */
-    public void inOrderTraversal(List<Node> nodes) {
-        inOrderTraversal(nodes.get(0));
-    }
-
-    /**
-     * @Description: 后序遍历 - 逻辑
-     */
-    private void postOrderTraversal(Node curr) {
-        if (curr == null) {
-            return;
-        }
-
-        postOrderTraversal(curr.left);
-        postOrderTraversal(curr.right);
-        System.out.print(curr.data + "\t");
-    }
-
-    /**
-     * @Description: 后序遍历
-     */
-    public void postOrderTraversal(List<Node> nodes) {
-        postOrderTraversal(nodes.get(0));
-    }
-
-    /**
-     * @Description: 层序遍历
-     */
-    public void levelOrderTraversal(List<Node> nodes) {
-        Node curr = nodes.get(0);
-
-        if (curr == null) {
-            return;
+    private Map<Character, String> getHuffmanCode(Node node) {
+        if (node == null) {
+            return null;
         }
 
         Queue<Node> queue = new LinkedList<>();
 
-        // 当前结点入队
-        queue.add(curr);
+        Map<Character, String> charToCode = new HashMap<>();
+
+        // 根结点入队
+        queue.add(node);
 
         while (!queue.isEmpty()) {
-            curr = queue.remove();
+            node = queue.remove();
 
-            System.out.print(curr.data + "\t");
-
-            if (curr.left != null) {
-                queue.add(curr.left);
+            // 添加叶子结点的编码
+            if (node.left == null && node.right == null) {
+                charToCode.put(node.data, node.code);
             }
 
-            if (curr.right != null) {
-                queue.add(curr.right);
+            if (node.left != null) {
+                node.left.code = node.code + "0";
+                queue.add(node.left);
             }
-        }
-    }
 
-    /**
-     * @Description: 对字符串进行哈夫曼编码
-     * @param: str 未编码的字符串
-     */
-    public void huffmanCoding(String str) {
-        System.out.println("未编码的字符串：" + str);
-
-        /* 处理字符串 */
-        str = str.replaceAll(" ", ""); // 去掉空格
-        str = str.replaceAll("[\\pP]", ""); // 去掉标点符号
-
-        /* 统计字符串中每一个字符出现次数的权重 */
-        char[] chars = str.toCharArray(); // 字符串 -> 字符数组
-
-        Map<Character, Integer> charToCount = new HashMap<>(); // 创建哈希表用于存放字符及对应出现次数
-
-        for (char item : chars) {
-            if (!charToCount.containsKey(item)) { // 如果之前没有出现过，则该键对应的值为1
-                charToCount.put(item, 1);
-            } else { // 如果之前出现过，则覆盖该键对应的值，直接自增
-                charToCount.put(item, charToCount.get(item) + 1);
+            if (node.right != null) {
+                node.right.code = node.code + "1";
+                queue.add(node.right);
             }
-        }
-
-        /* 根据哈希表中的键和值，用data和weight创建结点，并存入列表 */
-        List<Node> charNodes = new ArrayList<>();
-
-        for (Character data : charToCount.keySet()) {
-            charNodes.add(new Node(data, charToCount.get(data)));
-        }
-
-        /* 根据列表构建哈夫曼树 */
-        List<Node> huffmanCharTree = createHuffmanTree(charNodes);
-
-        /* 拿到编码后的字符表 */
-        Map<Character, String> characterCodeMap = getLeafCode(huffmanCharTree.get(0), "", new HashMap<>());
-
-        /* 替换字符串 */
-        for (Character character : characterCodeMap.keySet()) {
-            // 获取每个字符对应的编码
-            String characterCode = characterCodeMap.get(character);
-
-            System.out.println("character: " + character + " -> code: " + characterCode);
-
-            // 找到字符串中和表对应的key，用编码代替
-            str = str.replace(character.toString(), characterCode);
-        }
-
-        System.out.println("哈夫曼编码后的字符串：" + str);
-    }
-
-    /**
-     * @Description: 找到二叉树的叶子结点并生成哈夫曼编码
-     */
-    private Map getLeafCode(Node curr, String code, Map charToCode) {
-        // 向左搜寻完毕之后，需要记住上一个状态，即还未遍历左兄弟结点的状态，然后根据上一个状态开始向右寻找叶子结点并编码
-        String last = code;
-
-        if (curr.left == null && curr.right == null) {
-            charToCode.put(curr.data, code); // 如果遍历到叶子结点，就将叶子结点的数据和权值存入表中
-            return charToCode;
-        }
-
-        if (curr.left != null) {
-            code = last + "0"; // 如果向左，就加0
-            getLeafCode(curr.left, code, charToCode);
-        }
-
-        if (curr.right != null) {
-            code = last + "1"; // 如果向右，就加1
-            getLeafCode(curr.right, code, charToCode);
         }
 
         return charToCode;
+    }
+
+    /**
+     * @Description: 递归计算哈夫曼编码
+     */
+    private Map<Character, String> getHuffmanCode(Node node, Map<Character, String> charToCode) {
+        // 添加叶子结点的编码
+        if (node.left == null && node.right == null) {
+            charToCode.put(node.data, node.code);
+        }
+
+        if (node.left != null) {
+            node.left.code = node.code + "0";
+            getHuffmanCode(node.left, charToCode);
+        }
+
+        if (node.right != null) {
+            node.right.code = node.code + "1";
+            getHuffmanCode(node.right, charToCode);
+        }
+
+        return charToCode;
+    }
+
+    /**
+     * @Description: 根据字符及其出现频率计算哈夫曼编码
+     */
+    public String huffmanCoding(Map<Character, Integer> charToFreq, String str) {
+        // 根据每个字符及其频率创建结点，并存入数组
+        Node[] nodes = new Node[charToFreq.size()];
+
+        for (Character data : charToFreq.keySet()) {
+            nodes[this.size++] = new Node(data, charToFreq.get(data));
+        }
+
+        // 构建哈夫曼树
+        Node huffmanTree = buildHuffmanTree(nodes);
+
+        // 计算哈夫曼编码
+        Map<Character, String> charCode = getHuffmanCode(huffmanTree, new HashMap<>());
+
+        // 替换字符串
+        for (Character character : charCode.keySet()) {
+            String code = charCode.get(character);
+
+            System.out.println("char: " + character + " -> code: " + code);
+
+            str = str.replace(String.valueOf(character), code);
+        }
+
+        return str;
     }
 }
--- basis/src/datastructure/tree/huffmantree/HuffmanTreeTest.java
+++ basis/src/datastructure/tree/huffmantree/HuffmanTreeTest.java
@@ -1,46 +1,46 @@
 package datastructure.tree.huffmantree;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * @Description: 哈夫曼树、哈夫曼编码 - 测试类
+ * @Description: 哈夫曼树及哈夫曼编码 - 测试类
  */
 public class HuffmanTreeTest {
+    /**
+     * @Description: 预处理字符串，并计算字符串中每个字符的频率
+     */
+    private static Map<Character, Integer> getCharFreq(String str) {
+        str = str.replaceAll(" ", ""); // 去掉空格
+        str = str.replaceAll("[\\pP]", ""); // 去掉标点符号
+
+        char[] chars = str.toCharArray();
+
+        Map<Character, Integer> charToFreq = new HashMap<>();
+
+        for (char item : chars) {
+            if (!charToFreq.containsKey(item)) { // 如果之前没有出现过，则该键出现的次数为1
+                charToFreq.put(item, 1);
+            } else { // 如果之前出现过，则该键出现的次数+1
+                charToFreq.put(item, charToFreq.get(item) + 1);
+            }
+        }
+
+        return charToFreq;
+    }
+
     public static void main(String[] args) {
-        // 《清华数据结构》 P146
-        HuffmanTree huffmanTree = new HuffmanTree();
+        // 《清华数据结构》 P146  a:7, b:5, c:2, d:4
+        String str = "aaaaaaabbbbbccdddd";
 
-        List<HuffmanTree.Node> nodes = new ArrayList<>();
+        Map<Character, Integer> charFreq = getCharFreq(str);
 
-        nodes.add(huffmanTree.new Node("a", 7));
-        nodes.add(huffmanTree.new Node("b", 5));
-        nodes.add(huffmanTree.new Node("c", 2));
-        nodes.add(huffmanTree.new Node("d", 4));
+        HuffmanTree huffmanTree = new HuffmanTree(charFreq.size());
 
-        List<HuffmanTree.Node> huffmanNodes = huffmanTree.createHuffmanTree(nodes);
+        String codedStr = huffmanTree.huffmanCoding(charFreq, str);
 
-        System.out.print("前序遍历：");
-        huffmanTree.preOrderTraversal(huffmanNodes);
-
-        System.out.println();
-
-        System.out.print("中序遍历：");
-        huffmanTree.inOrderTraversal(huffmanNodes);
-
-        System.out.println();
-
-        System.out.print("后序遍历：");
-        huffmanTree.postOrderTraversal(huffmanNodes);
-
-        System.out.println();
-
-        System.out.print("层序遍历：");
-        huffmanTree.levelOrderTraversal(huffmanNodes);
-
-        System.out.println();
-
-        // a:7, b:5, c:2, d:4 转为哈夫曼编码
-        huffmanTree.huffmanCoding("aaaaaaabbbbbccdddd");
+        System.out.println("原始字符串：" + str);
+        System.out.println("编码后字符串：" + codedStr);
     }
 }
